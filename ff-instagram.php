be60abcb7b0a4071833c629a55cb9044903ffb8f
Alistair Ponce <alistair.ponce@gmail.com>
2024-08-05 15:26:53 +0900

    update

--- ff-instagram.php
+++ ff-instagram.php
@@ -14,7 +14,7 @@ if ( ! defined( 'ABSPATH' ) ) die();
 define( 'FF_INSTA', [
 	'app_id' => '1001361776949629',
 	'app_secret' => '3099b4f474f269216c2ebadfb765a7fd',
-	'redirect_uri' => 'https://bigdaddy2024.wpengine.com/instagram-auth/',
+	'redirect_uri' => 'https://fivebyfive.com.au/instagram-auth/',
 	'access_token' => '',
 ]);
 
